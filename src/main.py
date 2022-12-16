4e7f5059e520d755dabc4b4b34d1f0591e2a2f3c
Maíra Canal <mairacanal@riseup.net>
2022-12-16 18:58:53 -0300

    Remove components from previous project

--- src/main.py
+++ src/main.py
@@ -86,26 +86,8 @@ if __name__ == "__main__":
     glfw.show_window(window)
 
     # Cria listener para teclas e define ações para cada uma delas
-    def key_event(window,key,scancode,action,mods):
-        # Movimentos do barco com as setas
-        if key == 265:
-            scene.objects["Boat"].translate( 0.00, 0.01) # Cima
-            scene.objects["Boat"].scale( 0.99, 0.99) # Fica menor == mais longe
-        if key == 264:
-            scene.objects["Boat"].translate( 0.00,-0.01) # Baixo
-            scene.objects["Boat"].scale( 1.01, 1.01) # Fica maior == mais perto
-        if key == 263: scene.objects["Boat"].translate(-0.01, 0.00) # Esquerda
-        if key == 262: scene.objects["Boat"].translate( 0.01, 0.00) # Direita
-
-        # Movimentos do Sol com aswd
-        if key == 87: scene.objects["Sun"].scale(1.02, 1.02) # Aumenta
-        if key == 83: scene.objects["Sun"].scale(0.98, 0.98) # Diminui
-        if key == 68: scene.objects["Sun"].translate( 0.01, 0.00) # Direita
-        if key == 65: scene.objects["Sun"].translate(-0.01, 0.00) # Esquerda
-
-        # Movimentos da Hélice com qe
-        if key == 81: scene.objects["Helix"].rotate(0.03) # roda
-        if key == 69: scene.objects["Helix"].rotate(-0.03) # roda
+    def key_event(window, key, scancode, action, mods):
+        pass
 
     glfw.set_key_callback(window,key_event)
 
@@ -118,11 +100,6 @@ if __name__ == "__main__":
 
         # Set sky color
         glClearColor(0.529, 0.808, 0.922, 1.0)
-
-        # Move clouds through the sky
-        scene.objects["Cloud1"].translate(0.001, 0.0)
-        scene.objects["Cloud2"].translate(0.001, 0.0)
-        scene.objects["Cloud3"].translate(0.001, 0.0)
 
         scene.draw()
 
--- src/scene.py
+++ src/scene.py
@@ -4,16 +4,6 @@
 # Created Date: 18/09/2022
 # ---------------------------------------------------------------------------
 
-from houses import RedHouse, GreenHouse
-from mountains import Mountains
-from sun import Sun
-from field import Field
-from river import River
-from tree import ScotchPineTree, SugarPineTree
-from boat import Boat
-from clouds import Cloud
-from windmill import Windmill
-from helix import Helix
 from object import Color, Coordinates
 
 class Scene:
@@ -30,21 +20,6 @@ class Scene:
     def __init__(self, program):
         # List of all the objects in the scene
         self.objects = {
-            "Sun": Sun(program),
-            "River": River(program),
-            "Field": Field(program),
-            "Mountains": Mountains(program),
-            "RedHouse": RedHouse(program),
-            "GreenHouse": GreenHouse(program),
-            "SugarTreePine1": SugarPineTree(program),
-            "ScotchTreePine1": ScotchPineTree(program, Coordinates( -0.3 , 0.0)),
-            "ScotchTreePine2": ScotchPineTree(program, Coordinates( 0.89 , -0.24)),
-            "Cloud1": Cloud(program, 0.0),
-            "Cloud2": Cloud(program, 1.1),
-            "Cloud3": Cloud(program, 1.5),
-            "Windmill": Windmill(program, Coordinates( 0.27 , -0.42)),
-            "Helix": Helix(program, Coordinates( -0.03 , 0.03)),
-            "Boat": Boat(program, Coordinates( 0.65 , -0.75))
         }
 
     def prepare(self):
